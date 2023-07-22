76dbc449b7ad550ed23ac86ea920ba1b28fbbcc9
ConfusedDiscord <75702608+ConfusedDiscord@users.noreply.github.com>
2023-07-22 18:43:10 +0300

    final changes for now?

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Disclamer
+Currently Updated With GoldHen 2.4b14
 This is only a host to load the GoldHen Payload.
 Use this at your OWN risk, No modifications were done to the Goldhen Payload By [SiSTR0](https://github.com/SiSTR0)
 This Is not heavily worked on, as it takes a lot to maintain. Its more of a fun project.
--- common/functions.js
+++ common/functions.js
@@ -1,12 +1,7 @@
 function payload_finished(payload)
 {
 	setTimeout(function(){document.getElementById("progress").innerHTML="Load Successful!!";}, 3000);
-	if(payload == "binloader"){
-		setTimeout(function(){document.getElementById("progress").innerHTML="Awaiting Payload!! Send Payload To Port 9021"; }, 7000);
-	} else{
-		setTimeout(function(){document.getElementById("progress").innerHTML="PS4 Jailbreak 6.72 Payload Loaded Succesfully !!"; }, 7000);
-	}
-	if(payload == "hen" || payload == "mirahen" || payload == "goldhen" || payload == "miraunofficial"){
+	if(payload == "goldhen"){
 		localStorage.Fail--;
 		localStorage.Success++;
 		document.getElementById("success").innerHTML="Success: "+localStorage.Success;
@@ -15,52 +10,9 @@ function payload_finished(payload)
 
 function triggerFunction(payload){
 	preloader();
-	if(payload == "app2usb"){
-		app2usb();
-	}else if(payload == "backup"){
-		backup();
-	}else if(payload == "disableupdates"){
-		disableupdates();
-	}else if(payload == "dumper"){
-		dumper();
-	}else if(payload == "enablebrowser"){
-		enablebrowser();
-	}else if(payload == "enableupdates"){
-		enableupdates();
-	}else if(payload == "fanthreshold"){
-		fanthreshold();
-	}else if(payload == "ftp"){
-		ftp();
-	}else if(payload == "hen"){
-		localStorage.Fail++;
-		hen();
-	}else if(payload == "historyblocker"){
-		historyblocker();
-	}else if(payload == "kernelclock"){
-		kernelclock();
-	}else if(payload == "kerneldumper"){
-		kerneldumper();
-	}else if(payload == "linuxloader"){
-		linuxloader();
-	}else if(payload == "mirahen"){
-		localStorage.Fail++;
-		mirahen();
-	}else if(payload == "goldhen"){
+	if(payload == "goldhen"){
 		localStorage.Fail++;
 		goldhen();
-	}else if(payload == "miraunofficial"){
-		localStorage.Fail++;
-		miraunofficial();
-	}else if(payload == "ps4debug"){
-		ps4debug();
-	}else if(payload == "restore"){
-		restore();
-	}else if(payload == "rifrenamer"){
-		rifrenamer();
-	}else if(payload == "todex"){
-		todex();
-	}else if(payload == "webrte"){
-		webrte();
 	}
 	loader();
 	payload_finished(payload);
